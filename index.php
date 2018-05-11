a1c32cd070aac8349ff4e5987f953373b8859bbe
Jummis <juliazimmergren@hotmail.com>
2018-05-11 13:53:30 +0200

    Index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE>
+
+<html>
+
+    <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Hälsocoachen</title>
+    <link rel = "stylesheet" href = "main.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">n
+    </head>
+        <body>
+<div id = "header">
+<h>Hälsocoachen</h1>
+
+</div>
+
+ <ul>
+  <li><a href="default.asp">VAD ÄR HÄLSOCOACHING?</a></li>
+  <li><a href="news.asp">OM OSS</a></li>
+  <li><a href="contact.asp">LOGGA IN/REGISTRERA</a></li>
+</ul> 
+
+<img src="../Projekt/img/headertest2.jpg">
+
+<p> Här ska det stå något </p>
+
+        </body>
+
+</html>
