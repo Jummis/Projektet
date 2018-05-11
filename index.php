ce45645dc9add1f22da6c685fa98133ea974ca09
elinsvante <elin.svantesson@outlook.com>
2018-05-11 14:07:32 +0200

    Ändrade ID för bilderna

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
 
     <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>Hälsocoachennnnnn</title>
+    <title>Hälsocoachen</title>
     <link rel = "stylesheet" href = "main.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">n
     </head>
         <body>
 <div id = "header">
 <h>Hälsocoachen</h1>
+<img id = "headerIMG" src ="https://preview.ibb.co/duvS6J/lansering_halsostrategerna_st_2e16d0ba_fill_825x380_c100.jpg">
 
 </div>
 
@@ -20,9 +21,12 @@
   <li><a href="contact.asp">LOGGA IN/REGISTRERA</a></li>
 </ul> 
 
-<img src="../Projekt/img/headertest2.jpg">
+<div id = body>
+<img id = "frontIMG" src="../Projekt/img/headertest2.jpg">
 
 <p> Här ska det stå något </p>
+
+<div>
 
         </body>
 
