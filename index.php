6d7ccacfecf0c366cf745bce2859c4926b8bfb9a
elinsvante <elin.svantesson@outlook.com>
2018-05-11 14:19:44 +0200

    vad är detta?

--- index.php
+++ index.php
@@ -6,19 +6,18 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>Hälsocoachen</title>
     <link rel = "stylesheet" href = "main.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">n
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
         <body>
 <div id = "header">
 <h>Hälsocoachen</h1>
-<img id = "headerIMG" src ="https://preview.ibb.co/duvS6J/lansering_halsostrategerna_st_2e16d0ba_fill_825x380_c100.jpg">
 
 </div>
 
  <ul>
   <li><a href="default.asp">VAD ÄR HÄLSOCOACHING?</a></li>
   <li><a href="news.asp">OM OSS</a></li>
-  <li><a href="contact.asp">LOGGA IN/REGISTRERA</a></li>
+  <li><a href="login.php">LOGGA IN/REGISTRERA</a></li>
 </ul> 
 
 <div id = body>
