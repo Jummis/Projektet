af983a4a73a724cad93e4114c1d80b0c0be4f3f7
Jummis <juliazimmergren@hotmail.com>
2018-05-11 14:24:08 +0200

    Ny

--- index.php
+++ index.php
@@ -6,28 +6,25 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>Hälsocoachen</title>
     <link rel = "stylesheet" href = "main.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">n
     </head>
         <body>
 <div id = "header">
-<<<<<<< HEAD
-<h>Hälsocoachen</h1>
-=======
 <h>HÄLSOCOACHEN</h1>
 <img id = "headerIMG" src ="https://preview.ibb.co/duvS6J/lansering_halsostrategerna_st_2e16d0ba_fill_825x380_c100.jpg">
->>>>>>> c46ddb3d54ac21447a8a74ea2bbf47ab3cbdaf1b
 
 </div>
 
  <ul>
   <li><a href="default.asp">VAD ÄR HÄLSOCOACHING?</a></li>
   <li><a href="news.asp">OM OSS</a></li>
-  <li><a href="login.php">LOGGA IN/REGISTRERA</a></li>
+  <li><a href="contact.asp">LOGGA IN/REGISTRERA</a></li>
 </ul> 
 
 <div id = body>
 <img id = "frontIMG" src="../Projektet/img/header.jpg">
 
+<img id = "middleIMG" src="../Projektet"
 <p> Här ska det stå något </p>
 
 <div>
