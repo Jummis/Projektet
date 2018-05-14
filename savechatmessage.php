b9f90b924b99f6f1725b3841bd2357ceff4ceab3
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-14 12:19:11 +0200

    Börjat lägga in php för att spara chatt

--- savechatmessage.php
+++ savechatmessage.php
@@ -12,4 +12,12 @@ if ($connection->connect_error)
     die ("Connection failed : ".$connection.connect_error);
     }
 
+ if ($_SERVER["REQUEST_METHOD"] == 'POST'){
+        $uID = $connect->real_escape_string($_SESSION['User']);
+        $message = $connect->real_escape_string($_POST['usermsg']);
+
+      $insertmessage = "INSERT INTO chatt(submitted, from_id, message) VALUES (CURRENT_TIMESTAMP, '$uID', '$message');";
+      $connect->query($insertmessage); 
+ }
+
 ?>
