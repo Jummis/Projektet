8df13be75d03bb7ccb6aef75c4743dab9cdd68a9
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-14 15:05:23 +0200

    Mellanrum mellan val-knappar

--- chatOptions.php
+++ chatOptions.php
@@ -25,11 +25,11 @@
 
         <div class="optionButtons">
        <a href='chatt.php'> <input type="submit" class="button" value="KOST"><br>
-        <input type="submit" class="button" value="TRÄNING"<br>
-        <input type="submit" class="button" value="STRESS" <br>
-        <input type="submit" class="button" value="SÖMN" <br>
-        <input type="submit" class="button" value="ALKOHOL/TOBAK" <br>
-        <input type="submit" class="button" value="ALLMÄNT" <br> </a>
+        <input type="submit" class="button" value="TRÄNING"><br>
+        <input type="submit" class="button" value="STRESS"><br>
+        <input type="submit" class="button" value="SÖMN"><br>
+        <input type="submit" class="button" value="ALKOHOL/TOBAK"><br>
+        <input type="submit" class="button" value="ALLMÄNT"><br> </a>
         </div>
      </body>
 
