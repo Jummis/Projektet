8867b3ab894e9271afe8870ec224802ad020251b
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-14 10:26:51 +0200

    Välkomstext paragraf

--- userHome.php
+++ userHome.php
@@ -14,11 +14,13 @@
 <img id = "headerIMG" src="https://preview.ibb.co/kgnb5d/HEADERBILD.jpg">
 
  <ul>
-  <li><a href="default.asp">VAD ÄR HÄLSOCOACHING?</a></li>
-  <li><a href="news.asp">OM OSS</a></li>
+  <li><a href="default.asp">MINA SIDOR</a></li>
+  <li><a href="news.asp">CHATT</a></li>
 </ul> 
-<h1> Welcome <?php echo $_SESSION['User']; ?> </h1>
-<p1> You are logged in </p1>
+<h1> Hej <?php echo $_SESSION['User']; ?> </h1>
+<p1> Hur kan vi hjälpa dig idag?<br>
+     Tryck på chatta och berätta för oss vad<br>
+     vi kan göra för dig. </p1>
 <br><br>
 <a href = "logout.php"><input type=submit name=logout value=Logout></a>
 </div>
