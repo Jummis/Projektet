d188c8c0474f6e58d2d969d63bae1f3871597c12
Jummis <juliazimmergren@hotmail.com>
2018-05-14 15:18:13 +0200

    Chatten funkar

--- chatt.php
+++ chatt.php
@@ -24,21 +24,41 @@
         </div>
 
         <div id= "welcome">
-            <h1> Välkommen till chatten <?php echo $_SESSION['User']; ?> </h1>
+            <h1> Välkommen till chatten <?php echo $_SESSION['User']; ?>!</h1>
             <p1> Här kan du skicka ett meddelande till någon av våra hälsocoacher<br>
                  En coach kommer snart kontakta dig. </p1>
             <br><br>
         </div>
 
         <div id="chatcontainer">
-        <div id="chatbox"></div>
-            <form name="chat" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
 
-            <input type="text" placeholder="Vänligen skriv ditt meddelande här" name="usermsg" id="usermsg"/>
-            <input type="submit" name=submitmsg value=SKICKA id=sendmessage/>
-            </form>
+        <div id="chatbox">
+            <?php
+                $getdata = "SELECT from_id, message FROM Chatt";
+
+                // sparar resultatet av queryn i en variabel
+                $result = $connection->query($getdata);
+
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    echo "<h5>".$row["from_id"]."</h5>";
+                    echo $row["message"] . "<br>";
+                    echo "<p4>". "Tid: ". "</p4>";
+                    echo "<hr>";
+                }
+        
+            ?>
+
         </div>
- 
+            <form name="chat" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+            <!-- hämtar namn och kommentar från databasen -->
+
+            <textarea placeholder="Vänligen skriv ditt meddelande här" name="usermsg" id="usermsg"></textarea><br>
+            <input type="submit" name="submitmsg" value="SKICKA" id="sendmessage"/>
+
+            </form>
+
+        </div>
 
     </body>
 
