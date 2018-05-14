ac9f6ab3902ca2cab1895d1d602204971e6cfd95
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-14 12:29:11 +0200

    inkluderade session i sparachatt

--- savechatmessage.php
+++ savechatmessage.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once 'session.php';
+
 $uname = "dbtrain_763";
 $pass = "yvgjnd";
 $host = "dbtrain.im.uu.se";
@@ -13,10 +15,10 @@ if ($connection->connect_error)
     }
 
  if ($_SERVER["REQUEST_METHOD"] == 'POST'){
-        $uID = $connect->real_escape_string($_SESSION['User']);
+        //$uID = $connect->($_SESSION['User']);
         $message = $connect->real_escape_string($_POST['usermsg']);
 
-      $insertmessage = "INSERT INTO chatt(submitted, from_id, message) VALUES (CURRENT_TIMESTAMP, '$uID', '$message');";
+      $insertmessage = "INSERT INTO chatt(submitted, from_id, message) VALUES (CURRENT_TIMESTAMP, '$_SESSION['User'] ', '$message');";
       $connect->query($insertmessage); 
  }
 
