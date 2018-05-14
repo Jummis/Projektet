84ca4e6e253769fcb8106bfd11a076a1af7dbcf1
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-14 10:33:44 +0200

    Chattknapp

--- userHome.php
+++ userHome.php
@@ -16,14 +16,14 @@
  <ul>
   <li><a href="default.asp">MINA SIDOR</a></li>
   <li><a href="news.asp">CHATT</a></li>
-  <li><a href="ex.asp">LOGGA UT</a></li>
+  <li><a href="logout.php">LOGGA UT</a></li>
 </ul> 
 <h1> Hej <?php echo $_SESSION['User']; ?> </h1>
 <p1> Hur kan vi hjälpa dig idag?<br>
      Tryck på chatta och berätta för oss vad<br>
      vi kan göra för dig. </p1>
 <br><br>
-<a href = "logout.php"><input type=submit name=logout value=Logout></a>
+<a href = "chatt.php"><input type=submit name=chattbutton value=CHATTA></a>
 </div>
 
 </body>
