2e22510ff8a128aab4c9c5ecb0011b2e8b09b369
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-14 10:21:54 +0200

    Lagt in header i userHome

--- userHome.php
+++ userHome.php
@@ -7,16 +7,21 @@
 <head>
     <title> Hälsocoachen - homepage </title>
     <link rel = "stylesheet" href = "main.css">
+</head>
 
 <body>
 <div id = "header">
+<img id = "headerIMG" src="https://preview.ibb.co/kgnb5d/HEADERBILD.jpg">
+
+ <ul>
+  <li><a href="default.asp">VAD ÄR HÄLSOCOACHING?</a></li>
+  <li><a href="news.asp">OM OSS</a></li>
+</ul> 
 <h1> Welcome <?php echo $_SESSION['User']; ?> </h1>
 <p1> You are logged in </p1>
 <br><br>
 <a href = "logout.php"><input type=submit name=logout value=Logout></a>
-
 </div>
 
 </body>
-</head>
 </html>
