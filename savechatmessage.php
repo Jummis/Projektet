7a7f5c14f56e1ee3d0e3bcf3862853f2c4c6b82c
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-14 13:52:44 +0200

    Spara meddelanden

--- savechatmessage.php
+++ savechatmessage.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once 'connection.php';
 include_once 'session.php';
 
 $uname = "dbtrain_763";
@@ -15,10 +15,9 @@ if ($connection->connect_error)
     }
 
  if ($_SERVER["REQUEST_METHOD"] == 'POST'){
-        //$uID = $connect->($_SESSION['User']);
         $message = $connect->real_escape_string($_POST['usermsg']);
 
-      $insertmessage = "INSERT INTO chatt(submitted, from_id, message) VALUES (CURRENT_TIMESTAMP, '$_SESSION['User'] ', '$message');";
+      $insertmessage = "INSERT INTO chatt(from_id, message) VALUES ('$_SESSION['User'] ', '$message');";
       $connect->query($insertmessage); 
  }
 
