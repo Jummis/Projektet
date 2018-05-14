476510276d0b741a53161857da2de42b25a06102
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-14 14:25:51 +0200

    Knappar till val av hjälp

---
+++ chatOptions.php
@@ -0,0 +1,36 @@
+<html>
+
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>Hälsocoachen Chatt</title>
+        <link rel = "stylesheet" href = "main.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id = "header">
+            <img id = "headerIMG" src="https://preview.ibb.co/kgnb5d/HEADERBILD.jpg">
+            <ul>
+            <li><a href="default.asp">MINA SIDOR</a></li>
+            <li><a href="news.asp">CHATT</a></li>
+            <li><a href="logout.php">LOGGA UT</a></li>
+            </ul> 
+        </div>
+
+        <div id= "welcome">
+            <h1> Vad vill du ha hjälp med idag? </h1>
+            <p1> Vänligen välj ett av nedanstående alternativ.</p1>
+            <br><br>
+        </div>
+
+        <div id= "optionButtons">
+        <button>KOST</button>
+        <button>TRÄNING</button>
+        <button>ALKOHOL/TOBAK</button>
+        <button>STRESS</button>
+        <button>SÖMN</button>
+        <button>ALLMÄNT</button>
+        </div>
+     </body>
+
+</html>
