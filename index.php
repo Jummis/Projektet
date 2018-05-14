248c9543506d56e0f4ab370d6c7e228398da4660
Jummis <juliazimmergren@hotmail.com>
2018-05-14 10:54:24 +0200

    Ändrat css lite

--- index.php
+++ index.php
@@ -9,10 +9,13 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
         <body>
+                
 
-<img id = "headerIMG" src="https://preview.ibb.co/kgnb5d/HEADERBILD.jpg">
+
+<!--<img id = "headerIMG" src="https://preview.ibb.co/kgnb5d/HEADERBILD.jpg">-->
 
  <ul>
+  <h>HÄLSOCOACHEN</h>
   <li><a href="default.asp">VAD ÄR HÄLSOCOACHING?</a></li>
   <li><a href="news.asp">OM OSS</a></li>
   <li><a href="login.php">LOGGA IN/REGISTRERA</a></li>
@@ -21,6 +24,11 @@
 <p> Här ska det stå någon<br>
 bra text som ser snygg ut<br>
 med någon bra bild bakom</p>
+
+<div class="container">
+<img id="frukt" src="https://image.ibb.co/kRE98y/frukt.jpg">
+<div class="textcenter"> Lite text över bilden</div>
+</div>
 
 
         </body>
