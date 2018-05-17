4b56584769943c9442ab66c5c24d923759ee3887
elinsvante <elin.svantesson@outlook.com>
2018-05-17 10:54:11 +0200

    log + register

--- register.php
+++ register.php
@@ -22,42 +22,35 @@
     <div id="wrapper"><br>
     <img id = "user" src = "https://image.ibb.co/edWLgJ/default_user_image.png">
     <br><br>
-    <p2> Har du redan ett konto?<a href = 'login.php'><p2> Logga in </p2> </a><br><br>
+    <p3> Har du redan ett konto?<a href = 'login.php'><p3> Logga in </p3> </a><br><br>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 
                 <input type="text" name="firstname" title="firstname" style="color:#888;" 
-                placeholder="Förnamn" value = "<?php echo $fname;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)" />
-                <br><p2><?php echo $fname_error; ?> </p2><br>
+                placeholder="Förnamn" value = "<?php echo $fname;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)"><br><br>
 
                 <input type="text" name="lastname" title="lastname" style="color:#888;" 
-                placeholder="Efternamn" value = "<?php echo $lname;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)" />
-                <br><p2><?php echo $lname_error; ?> </p2><br>
+                placeholder="Efternamn" value = "<?php echo $lname;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)"/><br><br>
 
                 <input type="birth" name="birth" title="birth" style="color:#888;" 
                 placeholder="YYYY-MM-DD" value = "<?php echo $birthnumber;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)"><br><br>
 
-
-                <div class="row">
-                <div class="column">
+                <div class="rowRadio">
+                <div class="columnRadio">
                 <input type="radio" name="gender" id = "gender" value="Man" class = "radio"> <br>
                 Man
                 </div>
-                <div class="column">
+                <div class="columnRadio">
                 <input type="radio" name="gender" id = "gender" value="Kvinna" class = "radio"><br>
                  Kvinna
                 </div>
-                </div> 
+                </div>
                 
-            
-               
-
                 <input type="text" name="email" title="email" style="color:#888;" 
-                placeholder="E-postadress" value = "<?php echo $email;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)" />
-                <br><p2><?php echo $email_error; ?> </p2><br>
+                placeholder="E-postadress" value = "<?php echo $email;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)"/><br><br>
 
                 <input type="password" name="password" title="password" style="color:#888;" 
-                placeholder="Välj ett lösenord" value = "<?php echo $password;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)" />
-                <br><p2><?php echo $password_error; ?> </p2><br>
+                placeholder="Välj ett lösenord" value = "<?php echo $password;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)"/><br>
+                <br><br><p3><?php echo $field_error; ?> </p3><br>
 
                 <br>
 
