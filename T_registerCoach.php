0a7dbe649420a71ddd8b1aa6f4888f3a163cde7e
elinsvante <elin.svantesson@outlook.com>
2018-05-18 14:39:04 +0200

    skapa coach

--- T_registerCoach.php
+++ T_registerCoach.php
@@ -1,30 +1,26 @@
 <?php
     include_once 'connection.php';
-    include_once 'registerUser.php';
+    include_once 'T_register-process.php';
 
 ?>
 
 <html>
 <head>
     <title> Hälsocoachen - Registrering </title>
-    <link rel = "stylesheet" href = "assets/main.css">
+    <link rel = "stylesheet" href = "coach/assets/maincoach.css">
 </head>
 <body>
 
 <header>
  <ul>
 
-  <a id = "Logga" href="index.php">HÄLSOCOACHEN</a>
-  <li><a href="info.php">VAD ÄR HÄLSOCOACHING?</a></li>
-  <li><a href="omoss.php">OM OSS</a></li>
-  <li><a href="login.php">LOGGA IN/REGISTRERA</a></li>
+  <a id = "Logga">REGISTRERA NY HÄLSOCOACH</a>
   
 </ul>
 </header>
 
     <div id="wrapper1"><br>
     <img id = "user" src = "https://image.ibb.co/edWLgJ/default_user_image.png"><br><br>
-    <p3>Har du redan ett konto?</p3><p3><a href = 'login.php'> Logga in </p3> </a> <br><br>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 
                 <input type="text" id = "inputBox" name="firstname" title="firstname" style="color:#888;" 
@@ -42,7 +38,7 @@
                 Man
                 </div>
                 <div class="columnRadio">
-                <input type="radio" name="gender" id = "gender" value="Kvinna" class = "radio"><br>
+                <input type="radio" id = "inputBox" name="gender" id = "gender" value="Kvinna" class = "radio"><br>
                  Kvinna
                 </div>
                 </div>
@@ -50,7 +46,7 @@
                 <input type="text" id = "inputBox" name="email" title="email" style="color:#888;" 
                 placeholder="E-postadress" value = "<?php echo $email;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)"/><br><br>
 
-                <input type="password" id = "inputBox" name="password" title="password" style="color:#888;" 
+                <input type="password"id = "inputBox" name="password" title="password" style="color:#888;" 
                 placeholder="Välj ett lösenord" value = "<?php echo $password;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)"/><br>
                 <br><br><p3><?php echo $field_error; ?> </p3>
 
