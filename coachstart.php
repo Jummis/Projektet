7e11c33788d93f8ef2a62fea558e35dcc29ab1de
elinsvante <elin.svantesson@outlook.com>
2018-05-18 13:00:23 +0200

    ändrat fil

---
+++ coachstart.php
@@ -0,0 +1,59 @@
+<!DOCTYPE>
+
+<html>
+
+    <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Hälsocoachen</title>
+    <link rel = "stylesheet" href = "main.css">
+    <link href="https://fonts.googleapis.com/css?family=News+Cycle" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+    </head>
+    
+<body>
+<!--<img id = "headerIMG" src="https://preview.ibb.co/kgnb5d/HEADERBILD.jpg">-->
+
+<header>
+ <ul>
+
+  <a id = "Logga" href="index.php">HÄLSOCOACHEN</a>
+  <li><a href="info.php">VAD ÄR HÄLSOCOACHING?</a></li>
+  <li><a href="omoss.php">OM OSS</a></li>
+  <li><a href="login.php">LOGGA IN/REGISTRERA</a></li>
+  
+</ul>
+</header>
+
+<div class="box">
+<img id="splash" src="https://preview.ibb.co/kn6ZgJ/aa98c1180c514c4d94e23326888a25cc_2.png">
+<div class="PicText"> OPTIMERA DIN LIVSSTIL MED HJÄLP AV EN PROFFESIONELL HÄLSOCOACH</div>
+</div>
+
+
+<div id="box">
+<p id="textunderhead">Skapa ett konto hos oss idag och bli kontaktad av en coach.<br> Smidigt, enkelt och kostnadsfritt</p>
+</div>
+
+</body>
+
+<footer>
+<p id="kontaktrubrik">HÄLSOCOACHEN</p>
+<p id="kontaktuppg">Kontaktuppgifter</p>
+
+
+ <div class="row">
+  <div class="column">
+    <img src="https://image.ibb.co/bUdviy/phone_call_1.png"><br>
+    <p3> 0703457947 </p3>
+  </div>
+  <div class="column">
+    <img src="https://image.ibb.co/jSQmqd/envelope.png"><br>
+    <p3> halsocoachen@coach.se </p3>
+  </div>
+</div> 
+
+</footer>
+
+</html>
