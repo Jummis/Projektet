8d152b149d1d276a2bcf9bf37418cb0220c9fecd
elinsvante <elin.svantesson@outlook.com>
2018-05-18 14:46:53 +0200

    coachmypage

--- coach/coachMypage.php
+++ coach/coachMypage.php
@@ -5,15 +5,30 @@
     <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>Hälsocoachen</title>
-    <link rel = "stylesheet" href = "assets/main.css">
+    <link rel = "stylesheet" href = "assets/maincoach.css">
     <link href="https://fonts.googleapis.com/css?family=News+Cycle" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
     </head>
+    
+<body>
+<!--<img id = "headerIMG" src="https://preview.ibb.co/kgnb5d/HEADERBILD.jpg">-->
 
-    <body>
-    <a href="#">Liveforum</a></li>
-    <a href="#">Profil</a></li>
-    </body>   
+<header>
+ <ul>
+
+  <a id = "Logga" href="index.php">HÄLSOCOACHEN</a>
+  <li><a href="logoutCoach.php">LOGGA UT</a></li>
+  
+</ul>
+</header>
+
+<div class="box">
+<img id="splash" src="https://preview.ibb.co/kn6ZgJ/aa98c1180c514c4d94e23326888a25cc_2.png">
+<div class="PicText"> OPTIMERA DIN LIVSSTIL MED HJÄLP AV EN PROFFESIONELL HÄLSOCOACH</div>
+</div>
+
+
+</body>
 </html>
