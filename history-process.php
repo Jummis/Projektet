cdf6a2c284829989996d281777c0f486bca4e1b3
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-24 10:11:09 +0200

    hämta coachdata i ifsats

--- history-process.php
+++ history-process.php
@@ -4,24 +4,29 @@ include 'connection.php';
             $_SESSION['ID']=$userID
             
             if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-                
+
                 if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['training'])){
                     $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=training AND $userID=clientID";
+                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=training"
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['food'])){
                     $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=food AND $userID=clientID";
+                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=food"
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['stress'])){
                     $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=stress AND $userID=clientID";
+                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=stress"
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['sleep'])){
                     $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=sleep AND $userID=clientID";
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['alcohol'])){
                     $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=alcohol AND $userID=clientID";
+                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=alcohol"
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['general'])){
                     $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=general AND $userID=clientID";
+                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=general"
                 }
 
                 $_SESSION['User']=$user
