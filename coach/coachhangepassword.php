8755521c4a3ac1b269ae1dcefc3b8f39b354082e
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-25 12:57:23 +0200

    försöker lösa updatera lösenord

--- coach/coachhangepassword.php
+++ coach/coachhangepassword.php
@@ -2,38 +2,39 @@
 include_once '../connection.php';
 include_once 'sessioncoach.php';
 
+function unique_salt() {
+    return substr(sha1(mt_rand()),0,22);
+}
+
+
     if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+
+        $passwordErr="";
 
         $oldpassword = $connection->real_escape_string($_POST['currentPassword']);
         $newpassword = $connection->real_escape_string($_POST['newPassword']);
         $repnewpasssword = $connection->real_escape_string($_POST['confirmPassword']); 
 
-        $getsalt = "SELECT salt FROM Coach WHERE email = '$Email'";
+        $getsalt = "SELECT salt FROM Coach WHERE coachId=3";
         $resgetsalt = $connection->query($getsalt); 
 
-        while ($row = $resgetsalt->fetch_row()){
-            $Salt = $row[0];
-        }
+        $Oldpassword = sha1($resgetsalt . $oldpassword);
 
-        $hash = sha1($Salt . $oldpassword);
+        $getoldpassword = "SELECT password FROM Coach WHERE coachID=3";
+        $oldpassworddb = $connection->query($getoldpassword); 
+        
+        if (empty($oldpassword) OR empty($newpassword) OR empty($repnewpassword)) {
+        $passwordErr = "Du måste fylla i alla fält!";
+        } 
 
-        $getoldpassword = "SELECT password FROM Coach WHERE coachID=";
-        $resgetpw = $connection->query($getoldpassword); 
-        $oldpassworddb =$resgetpw['password'];
-
-        if($oldpassword==$oldpassworddb){
+        if($Oldpassword==$oldpassworddb){
 
             if($newpassword==$repnewpasssword){
-                $passchange= "UPDATE Coach SET password='$newpassword' WHERE email='$Email'";
+                $passchange= "UPDATE Coach SET password='$newpassword' WHERE coachID=3";
+                $updatePassword=$connection->query($passchange);
             }
             else $passwordErr="lösenorden matchar inte!"; 
         }
         else $passwordErr="felaktigt lösenord"; 
-
-        if (empty($oldpassword) OR empty($newpassword) OR empty($repnewpassword)) {
-        $password_error = "Du måste fylla i alla fält!";
-        } 
-
-
     }
 ?>
