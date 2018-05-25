e8ef072e9d33385968d32bef281297914d149ae8
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-25 08:59:49 +0200

    fix

--- showHistory.php
+++ showHistory.php
@@ -1,7 +1,6 @@
 <?php   
     include_once 'connection.php';
     include_once 'session.php';
-   //include_once 'history-process.php';
 ?>
 
 <html>
@@ -33,28 +32,28 @@
             if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 
                 if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['training'])){
-                    $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=training AND clientID='$userID'";
-                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID=$userID AND forum_type=training";
+                    $getclientdata = "SELECT * FROM Client_Message WHERE forum_type=training AND $userID=clientID";
+                    $getcoachdata= "SELECT * FROM Coach_Message WHERE clientMsgID=$userID AND forum_type=training";
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['food'])){
-                    $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=food AND clientID='$userID'";
-                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=food";
+                    $getclientdata = "SELECT * FROM Client_Message WHERE forum_type=food AND $userID=clientID";
+                    $getcoachdata= "SELECT * FROM Coach_Message WHERE clientMsgID= AND forum_type=food";
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['stress'])){
-                    $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=stress AND clientID='$userID'";
-                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=stress";
+                    $getclientdata = "SELECT * FROM Client_Message WHERE forum_type=stress AND $userID=clientID";
+                    $getcoachdata= "SELECT * FROM Coach_Message WHERE clientMsgID= AND forum_type=stress";
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['sleep'])){
-                    $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=sleep AND clientID='$userID'";
-                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=sleep";
+                    $getclientdata = "SELECT * FROM Client_Message WHERE forum_type=sleep AND $userID=clientID";
+                    $getcoachdata= "SELECT * FROM Coach_Message WHERE clientMsgID= AND forum_type=sleep";
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['alcohol'])){
-                    $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=alcohol AND clientID='$userID'";
-                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=alcohol";
+                    $getclientdata = "SELECT * FROM Client_Message WHERE forum_type=alcohol AND $userID=clientID";
+                    $getcoachdata= "SELECT * FROM Coach_Message WHERE clientMsgID= AND forum_type=alcohol";
                 }
                    if ($_SERVER["REQUEST_METHOD"] == 'POST' and isset($_POST['general'])){
-                    $getclientdata = "SELECT submitted, datum, message_client FROM Client_Message WHERE forum_type=general AND clientID='$userID'";
-                    $getcoachdata= "SELECT submitted, datum, message_coach FROM Coach_Message WHERE clientMsgID= AND forum_type=general";
+                    $getclientdata = "SELECT * FROM Client_Message WHERE forum_type=general AND $userID=clientID";
+                    $getcoachdata= "SELECT * FROM Coach_Message WHERE clientMsgID= AND forum_type=general";
                 }
 
                 $userimg = '<img id ="userchatt" src="https://image.ibb.co/edWLgJ/default_user_image.png" alt="default_user_image">';
@@ -81,7 +80,6 @@
                     echo "<p4>" . $row["submitted"] . "</p4>"."<br>";
                     echo "<hr>";
                 }
-                header ("Location: showHistory.php");
             }
                 ?>
             </div>
