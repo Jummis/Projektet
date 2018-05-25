ee3c03140c9dc695b071abb9eb772d2c0ab3bb98
EmeliaKarlsson <38293626+EmeliaKarlsson@users.noreply.github.com>
2018-05-25 13:00:03 +0200

    kommentarer

--- coach/coachhangepassword.php
+++ coach/coachhangepassword.php
@@ -2,11 +2,6 @@
 include_once '../connection.php';
 include_once 'sessioncoach.php';
 
-function unique_salt() {
-    return substr(sha1(mt_rand()),0,22);
-}
-
-
     if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 
         $passwordErr="";
@@ -15,11 +10,14 @@ function unique_salt() {
         $newpassword = $connection->real_escape_string($_POST['newPassword']);
         $repnewpasssword = $connection->real_escape_string($_POST['confirmPassword']); 
 
+        //Hämtar saltet från db
         $getsalt = "SELECT salt FROM Coach WHERE coachId=3";
         $resgetsalt = $connection->query($getsalt); 
 
+        //saltar och hashar det angivna lösenordet
         $Oldpassword = sha1($resgetsalt . $oldpassword);
 
+        //Hämtar lösenordet från db
         $getoldpassword = "SELECT password FROM Coach WHERE coachID=3";
         $oldpassworddb = $connection->query($getoldpassword); 
         
@@ -27,9 +25,11 @@ function unique_salt() {
         $passwordErr = "Du måste fylla i alla fält!";
         } 
 
+        //Om det angivna lösenordet och lösenordet i db matchar
         if($Oldpassword==$oldpassworddb){
-
+            //Om det nya lösenordet och det repeterade lösenordet matchar
             if($newpassword==$repnewpasssword){
+                //Updatera lösenordet i db
                 $passchange= "UPDATE Coach SET password='$newpassword' WHERE coachID=3";
                 $updatePassword=$connection->query($passchange);
             }
