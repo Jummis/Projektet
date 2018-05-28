9db9a0e40bdf38ae324df69d4c6df353f1a1abb1
Jummis <juliazimmergren@hotmail.com>
2018-05-28 13:29:38 +0200

    ny knapp i forumen

--- userHome.php
+++ userHome.php
@@ -15,7 +15,7 @@
             <div id "logoAll">
                 <img id = "holdingHands" src = "assets/img/hands-holding-heart.png">
                 <a id = "Logga" href="userHome.php">HÄLSOCOACHEN</a>
-                <li><a href="myPage.php" class="active">MINA SIDOR</a></li>
+                <li><a href="myPage.php">MINA SIDOR</a></li>
                 <li><a href="chatOptions.php">LIVEFORUM</a></li>
                 <li><a href="logout.php">LOGGA UT</a></li>  
                 <li><a href=""> </a></li>
