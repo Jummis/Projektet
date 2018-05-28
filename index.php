45dbd5b063da7de4d7ff46483501e214bc830f9a
elinsvante <elin.svantesson@outlook.com>
2018-05-28 13:01:35 +0200

    header

--- index.php
+++ index.php
@@ -12,13 +12,11 @@
 
         <header>
             <ul>
-            <div id "logoAll">
                 <img id = "holdingHands" src = "assets/img/hands-holding-heart.png">
                 <a id = "Logga" href="index.php">HÄLSOCOACHEN</a>
                 <li><a href="info.php">VAD ÄR HÄLSOCOACHING?</a></li>
                 <li><a href="omoss.php">OM OSS</a></li>
                 <li><a href="login.php">LOGGA IN/REGISTRERA</a></li>
-            </div>
             </ul>
         </header>
 
