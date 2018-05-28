42fe2b115e95baf43bbf1ea8a18dfc6e01626e08
elinsvante <elin.svantesson@outlook.com>
2018-05-28 13:43:27 +0200

    ej

--- coach/coachstart.php
+++ coach/coachstart.php
@@ -6,47 +6,37 @@ include_once 'validateCoachLogIn.php';
 <html>
 
     <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>Hälsocoachen</title>
-    <link rel = "stylesheet" href = "assets/maincoach.css">
-    <link href="https://fonts.googleapis.com/css?family=News+Cycle" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>Hälsocoachen</title>
+        <link rel = "stylesheet" href = "assets/maincoach.css">
+        <link href="https://fonts.googleapis.com/css?family=News+Cycle" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     </head>
     
-<body>
-<!--<img id = "headerIMG" src="https://preview.ibb.co/kgnb5d/HEADERBILD.jpg">-->
+        <body>
 
-<header>
- <ul>
+          <header>
+                <ul>
+                  <a id = "Logga">VÄLKOMMEN HÄLSOCOACH</a> 
+                </ul>
+          </header>
 
-  <a id = "Logga">VÄLKOMMEN HÄLSOCOACH</a>
-  
-</ul>
-</header>
+                <div id = "wrapperstart"><br>
+                  <img id = "user" src = "https://image.ibb.co/edWLgJ/default_user_image.png"><br><br>
 
-<div id = wrapper><br>
-<img id = "user" src = "https://image.ibb.co/edWLgJ/default_user_image.png">
-<br><br>
+                  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST"><br>
+                        <input type="text" id = "passBox" name="email" title="email" style="color:#888;" 
+                        placeholder="E-postadress" value = "<?php echo $email;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)" />
+                        <br><p3><?php echo $email_error; ?> </p3>
 
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                        <input type="password" id="passBox" name="password" title="password" style="color:#888;" 
+                        placeholder="Lösenord" value = "<?php echo $password;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)" />
+                        <br> <p3> <?php echo $password_error; ?> </p3>
 
-                <br>
-                <input type="text" id = "inputBox" name="email" title="email" style="color:#888;" 
-                placeholder="E-postadress" value = "<?php echo $email;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)" />
-                <br><p3><?php echo $email_error; ?> </p3>
-
-                <input type="password" id="passBox" name="password" title="password" style="color:#888;" 
-                placeholder="Lösenord" value = "<?php echo $password;?>" onfocus="inputFocus(this)" onblur="inputBlur(this)" />
-                <br> <p3> <?php echo $password_error; ?> </p3>
-
-                <input type="submit" name="submit" id="submit" value="Logga in">
-        </form>
-
-</div>
-
-
-</body>
+                        <input type="submit" name="submit" id="submit" value="Logga in">
+                </form>
+          </div>
+        </body>
 
 </html>
